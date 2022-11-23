592c18917e21f434c85297b2b7f9d571918cfcff
megha <meghamini40@gmail.com>
2022-11-23 13:13:54 +0530

    initial commit

---
+++ src/Components/Album/Album.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+const Album = () => {
+  return (
+    <div className="container">
+      <img
+        src="https://i.scdn.co/image/ab67616d0000b2730f365e81ca21fd7a779dc58a"
+        width="200px"
+        alt=""
+      />
+      <h3>B Praak</h3>
+      <p>Filhaal 2</p>
+    </div>
+  );
+};
+export default Album;
